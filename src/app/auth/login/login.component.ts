a25f4bb83050d4c5bc488bcb26d70c1696d19bec
Logan Troyer <troyerlogan@gmail.com>
2020-02-17 22:35:18 -0500

    Fixed redirect after login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Auth } from 'aws-amplify';
 import {Router} from '@angular/router';
 import {Storage} from '@ionic/storage';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,17 @@ export class LoginComponent implements OnInit {
   password: string;
   error: string;
 
-  constructor(private router: Router, private storage: Storage) { }
+  constructor(private router: Router, private storage: Storage, private loadingController: LoadingController) { }
 
   ngOnInit() {
 
   }
 
-  onLogin(username: string, password: string) {
+  async onLogin(username: string, password: string) {
+    const loading = await this.loadingController.create({
+      message: 'Logging in...',
+    });
+    await loading.present();
     const user = {
       username,
       password
@@ -27,7 +32,8 @@ export class LoginComponent implements OnInit {
 
     Auth.signIn(user).then(response => {
       this.storage.set('goshen-id', response.attributes.sub);
-      this.router.navigate(['Home']);
+      loading.dismiss();
+      this.router.navigate(['/tabs/Home']);
     })
         .catch(err => this.error = err.message);
   }
